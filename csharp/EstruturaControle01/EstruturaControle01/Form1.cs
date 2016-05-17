cb75df768db78a630b299c4f05461e04a3cd027a
Heitor <hteixei@gmail.com>
2016-05-17 16:38:02 -0300

    Estruturas de controle 1

---
+++ csharp/EstruturaControle01/EstruturaControle01/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EstruturaControle01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double saldo = 100.0;
+            double valorSaque = 10.0;
+            
+            if(saldo >= valorSaque)
+            {
+                saldo -= valorSaque;
+                MessageBox.Show("Saque realizado com sucesso");
+            }
+            else
+            {
+                MessageBox.Show("Saldo Insuficiente");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double saldo = 5.0;
+            double valorSaque = 10.0;
+
+            if(saldo >= valorSaque)
+            {
+                saldo -= valorSaque;
+                MessageBox.Show("Saque realizado com sucesso");
+            }
+            else
+            {
+                MessageBox.Show("Saldo Insuficiente");
+            }
+        }
+    }
+}
