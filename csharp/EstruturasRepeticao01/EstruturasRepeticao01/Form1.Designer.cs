374e245df143ae8968fe901c5e3a264c4761e43e
Heitor Teixeira <hteixei@gmail.com>
2016-05-23 23:55:47 -0300

    Exercicio repeticao - opcional 5

--- csharp/EstruturasRepeticao01/EstruturasRepeticao01/Form1.Designer.cs
+++ csharp/EstruturasRepeticao01/EstruturasRepeticao01/Form1.Designer.cs
@@ -82,9 +82,9 @@
             // 
             this.button5.Location = new System.Drawing.Point(26, 173);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.Size = new System.Drawing.Size(96, 23);
             this.button5.TabIndex = 4;
-            this.button5.Text = "button5";
+            this.button5.Text = "divisiveis3epor4";
             this.button5.UseVisualStyleBackColor = true;
             this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
